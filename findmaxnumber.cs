a9ff978d1d603a0440cdb1202236aa7aeb4df826
sahanab2000 <85185990+sahanab2000@users.noreply.github.com>
2022-10-12 21:44:18 +0530

    Add files via upload

---
+++ findmaxnumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace FindMaximumNumberProject
+{
+    public class MaximumNumberCheck
+    {
+        private static readonly int thirdValue;
+
+        public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdvalue)
+
+        {
+
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+            firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdvalue) > 0 ||
+            firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdvalue) >= 0)
+            {
+                return firstValue;
+
+            }
+
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+
+            secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+            secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+
+
+
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+
+            thirdvalue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+
+            thirdvalue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+
+        }
+
+    }
+
+}
